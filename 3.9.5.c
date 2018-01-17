9ade6281c582cc2168629cf4e5581777f75ea61d
yf165 <35261313+yf165@users.noreply.github.com>
2018-01-17 14:28:14 +0800

    Rev1.1
    inet_ntop intet_pton

--- 3.9.5.c
+++ 3.9.5.c
@@ -6,9 +6,23 @@
 
 int main(void)
 {
+#if 0
 	in_addr_t addr = 0;
 	addr = inet_addr(IP);
 	printf("addr = 0x%x\n",addr);
+
+	int ret =0;
+	struct in_addr addr;
+	ret  = inet_pton(AF_INET, IP, &addr);
+	if(ret != 1){
+		printf("inet_pton eeor\n");
+		return -1;
+	}
+	printf("addr = 0x%x\n",addr.s_addr);
+#endif	
+	const char *inet_ntop(int af, const void *src,
+                             char *dst, socklen_t size);
+
 	return 0;
 
 }
